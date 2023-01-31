14fef193e8ce6baa6d4ddd3fbde7f7fec6e922e5
Guilherme S Beneti <guilhermesbeneti@Guilhermes-Air.local>
2023-01-31 10:35:24 -0300

    hr payroll created and implemented

--- hr-worker/src/main/java/com/beneti/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/beneti/hrworker/resources/WorkerResource.java
@@ -2,7 +2,6 @@ package com.beneti.hrworker.resources;
 
 import com.beneti.hrworker.entities.Worker;
 import com.beneti.hrworker.repositories.WorkerRepository;
-import org.hibernate.jdbc.Work;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
