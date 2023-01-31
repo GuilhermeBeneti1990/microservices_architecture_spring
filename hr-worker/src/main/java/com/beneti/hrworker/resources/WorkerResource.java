835eaeaedf38090c6c3aa079abb848d19d76f7ac
Guilherme S Beneti <guilhermesbeneti@Guilhermes-Air.local>
2023-01-31 10:53:51 -0300

    Feing Client implemented on hr-payroll instead Rest Template

--- hr-worker/src/main/java/com/beneti/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/beneti/hrworker/resources/WorkerResource.java
@@ -2,7 +2,10 @@ package com.beneti.hrworker.resources;
 
 import com.beneti.hrworker.entities.Worker;
 import com.beneti.hrworker.repositories.WorkerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,17 +19,22 @@ import java.util.Optional;
 @RequestMapping("/workers")
 public class WorkerResource {
 
+    private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
+
     @Autowired
     private WorkerRepository workerRepository;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
+        logger.info("Find All Worker");
+
         List<Worker> list = workerRepository.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Worker> findById(@PathVariable Long id) {
+        logger.info("Find Worker by ID ::: " + id);
         Optional<Worker> worker = workerRepository.findById(id);
         return ResponseEntity.ok().body(worker.orElse(null));
     }
